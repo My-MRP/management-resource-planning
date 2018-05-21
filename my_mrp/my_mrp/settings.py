5afcfe2231d1858b05ccbf9cc979fdae4561b753
George Ceja <net.george.sky@gmail.com>
2018-05-21 12:46:38 -0700

    Configured Postgres as Database.

--- my_mrp/my_mrp/settings.py
+++ my_mrp/my_mrp/settings.py
@@ -73,10 +73,24 @@ WSGI_APPLICATION = 'my_mrp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': 5432,
+        'TEST': {
+            'NAME': 'mrp_test',
+        }
     }
 }
 
