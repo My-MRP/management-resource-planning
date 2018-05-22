6a4fecd2af45a4169aba90c6889e1f898ce28dbe
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-21 20:20:01 -0700

    worked on the quote form

--- my_mrp/vehicle_quote/forms.py
+++ my_mrp/vehicle_quote/forms.py
@@ -7,14 +7,16 @@ class VehicleQuote(ModelForm):
     """Define the VehicleQuote form."""
 
     class Meta:
-        """Meta data for VehiucleQuote form."""
+        """Meta data for VehicleQuote form."""
 
         model = Vehicle
-        fields = ['Name', 'Model Name', 'Engine', 'Interior', 'Audio', 'Exterior', 'Wheels', 'Accessories', 'Date Created', 'Expiration Date']
+        fields = ['Name', 'Model', 'Engine', 'Interior', 'Audio', 'Exterior', 'Wheels', 'Accessories', 'Date Created', 'Expiration Date']
 
     def __init__(self, *args, **kwargs):
-        """Init for VehiucleQuote form."""
+        """Init for VehicleQuote form."""
         username = kwargs.pop('username')
+        model_name_choices = Vehicle.objects.all().values_list('model_name')
+
         super().__init__(*args, **kwargs)
         self.fields['Name'].queryset = forms.CharField(max_length=180, null=False)
         self.fields['Model'].queryset = Vehicle.objects.filter('model_name')
@@ -27,3 +29,41 @@ class VehicleQuote(ModelForm):
         self.fields['Date Created'].queryset = Vehicle.objects.filter(model.'accessories')
         self.fields['Expiration Date'].queryset = Vehicle.objects.filter(model.'accessories')
 
+model_name_choices = Vehicle.objects.all().values_list('model_name')
+engine_choices = Vehicle.objects.filter(model_name=Model).values_list('engine', flat=True)
+
+
+
+Name
+Model = forms.MultipleChoiceField(choices=model_name_choices)
+Engine = forms.MultipleChoiceField(choices=model_name_choices)
+Interior = forms.MultipleChoiceField(choices=model_name_choices)
+Audio = forms.MultipleChoiceField(choices=model_name_choices)
+Exterior = forms.MultipleChoiceField(choices=model_name_choices)
+Wheels = forms.MultipleChoiceField(choices=model_name_choices)
+Accessories = forms.MultipleChoiceField(choices=model_name_choices)
+Date Created
+Expiration Date'
+
+
+
+# BIRTH_YEAR_CHOICES = ('1980', '1981', '1982')
+# FAVORITE_COLORS_CHOICES = (
+#     ('blue', 'Blue'),
+#     ('green', 'Green'),
+#     ('black', 'Black'),
+# )
+
+# class SimpleForm(forms.Form):
+#     birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+#     favorite_colors = forms.MultipleChoiceField(
+#         required=False,
+#         widget=forms.CheckboxSelectMultiple,
+#         choices=FAVORITE_COLORS_CHOICES,
+#     )
+
+# User.objects.all().values('username')
+# >>> [{'username': u'u1'}, {'username': u'u2'}]
+
+# >>> User.objects.all().values_list('username')
+# >>> [(u'u1',), (u'u2',)]
