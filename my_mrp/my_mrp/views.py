04589a07737b65c6f8af8345e73110ad6f111adb
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-21 18:03:46 -0700

    removed code to get it working

--- my_mrp/my_mrp/views.py
+++ my_mrp/my_mrp/views.py
@@ -1,16 +1,13 @@
 """Define the My MRP home view."""
 
 from django.views.generic.base import TemplateView
-from .models import Quote
 
 
 class HomeView(TemplateView):
     """Make the HomeView class."""
 
-    pass
- 
-    # template_name = 'generic/home.html'
-    # context_object_name = 'quotes'
+    template_name = 'generic/home.html'
+    context_object_name = 'quotes'
 
     # def get_queryset(self, **kwargs):
     #     """Get the context to display."""
