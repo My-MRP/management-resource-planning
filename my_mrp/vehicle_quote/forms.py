43abb7516671efafbe55fc1f0e5a1454c7136d53
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-21 18:51:35 -0700

    added the quote model, form, and html

--- my_mrp/vehicle_quote/forms.py
+++ my_mrp/vehicle_quote/forms.py
@@ -1,25 +1,29 @@
 from django.forms import ModelForm
 from .product_vehicle.models import Vehicle
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class VehicleQuote(ModelForm):
     """Define the VehicleQuote form."""
 
     class Meta:
-        """Meta data for album form."""
+        """Meta data for VehiucleQuote form."""
 
         model = Vehicle
-        fields = ['name', 'model', 'engine', 'interior', 'audio', 'exterior', 'wheels', 'accessories']
+        fields = ['Name', 'Model Name', 'Engine', 'Interior', 'Audio', 'Exterior', 'Wheels', 'Accessories', 'Date Created', 'Expiration Date']
 
     def __init__(self, *args, **kwargs):
-        """Init for album form."""
+        """Init for VehiucleQuote form."""
         username = kwargs.pop('username')
         super().__init__(*args, **kwargs)
-        self.fields['name'].queryset = forms.CharField(max_length=180, null=False)
-        self.fields['model'].queryset = Vehicle.objects.filter('model_name')
-        self.fields['engine'].queryset = Vehicle.objects.filter(model.'engine')
-        self.fields['interior'].queryset = Vehicle.objects.filter(model.'interior')
-        self.fields['audio'].queryset = Vehicle.objects.filter(model.'audio')
-        self.fields['exterior'].queryset = Vehicle.objects.filter(model.'exterior')
-        self.fields['wheels'].queryset = Vehicle.objects.filter(model.'wheels')
-        self.fields['accessories'].queryset = Vehicle.objects.filter(model.'accessories')
+        self.fields['Name'].queryset = forms.CharField(max_length=180, null=False)
+        self.fields['Model'].queryset = Vehicle.objects.filter('model_name')
+        self.fields['Engine'].queryset = Vehicle.objects.filter(model.'engine')
+        self.fields['Interior'].queryset = Vehicle.objects.filter(model.'interior')
+        self.fields['Audio'].queryset = Vehicle.objects.filter(model.'audio')
+        self.fields['Exterior'].queryset = Vehicle.objects.filter(model.'exterior')
+        self.fields['Wheels'].queryset = Vehicle.objects.filter(model.'wheels')
+        self.fields['Accessories'].queryset = Vehicle.objects.filter(model.'accessories')
+        self.fields['Date Created'].queryset = Vehicle.objects.filter(model.'accessories')
+        self.fields['Expiration Date'].queryset = Vehicle.objects.filter(model.'accessories')
+
--- my_mrp/vehicle_quote/models.py
+++ my_mrp/vehicle_quote/models.py
@@ -1,18 +1,24 @@
 """Make the model for a vehicle quote."""
 
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 
+class VehicleQuote(models.Model):
+    """This is the Quote model."""
 
-# class VehicleQuote(models.Model):
-#     """This is the Quote model."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicle_quotes')
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicle_quotes')
+    model_name = models.CharField(max_length=100, null=False)
+    engine = models.CharField(max_length=100, null=False)
+    exterior_color = models.CharField(max_length=100, null=False)
+    wheels = models.CharField(max_length=100, null=False)
+    interior_package = models.CharField(max_length=100, null=False)
+    audio_system = models.CharField(max_length=100, null=False)
+    date_quoted = models.DateField(auto_now_add=True, null=False)
+    date_modified = models.DateField(auto_now=True)
+    expiration_date = models.DateField(auto_now=True, null=False)
 
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicle_quotes')
-#     name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicle_quotes')
-#     date_quoted = models.DateField(auto_now_add=True)
-#     date_modified = models.DateField(auto_now=True)
-
-#     def __str__(self):
-#         """Show a string representation of the title."""
-#         return '{}'.format(self.name)
+    def __str__(self):
+        """Show a string representation of the quote name."""
+        return '{}'.format(self.name)
