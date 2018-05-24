2792a2e1dfcb47121a59115c86a565ddb0dc078f
George Ceja <net.george.sky@gmail.com>
2018-05-24 14:51:12 -0700

    createGraph creates graph on homeView loadtime.

--- my_mrp/my_mrp/views.py
+++ my_mrp/my_mrp/views.py
@@ -6,33 +6,9 @@ from django.views.generic.base import TemplateView
 from vehicle_quote.models import VehicleQuote
 
 
-class HomeView(TemplateView):
-    """Make the HomeView class."""
-
-    template_name = 'generic/home.html'
-    context_object_name = 'quotes'
-
-    # def get_queryset(self, **kwargs):
-    #     """Get the context to display."""
-    #     username = self.request.user.get_username()
-    #     quotes = Quote.objects.filter(user__username=username)
-
-    #     return quotes
-
-    # def get_context_data(self, **kwargs):
-    #     """Filter the context for display."""
-    #     context = super().get_context_data(**kwargs)
-    #     quotes = context['quotes'][0]
-
-    #     context.update({
-    #         'quotes': quotes,
-    #     })
-
-    #     return context
-
-
 # Generates Graph based on Data.
 def createGraph():
+    """Creates Visual for most popular models quoted."""
     vehicles = {}
     colors = {}
     car_models = VehicleQuote.objects.all()
@@ -44,25 +20,35 @@ def createGraph():
     try:
         # Uses Plotly's API
         plotly.tools.set_credentials_file(username='kiirby', api_key='TdfP7ccHINP4BI76EDNj')
-
-        trace = go.Pie(labels=list(vehicles.keys()), values=list(vehicles.values()))
+        trace = go.Pie(
+            labels=list(vehicles.keys()),
+            values=list(vehicles.values())
+        )
         # GRAPH DIMENSION.
-        layout = go.Layout(title='Most popular Models', width=800, height=640, paper_bgcolor='rgb(27, 32, 46)')
+        layout = go.Layout(
+            title='Most Quoted Models',
+            width=800, height=640,
+            paper_bgcolor='rgb(27, 32, 46)',
+            font=dict(color='rgb(52, 156, 134)', size=30)
+        )
         graph = go.Figure(data=[trace], layout=layout)
-
         # Creates graph and saves it to the project.
-        print('----------------------------------------------')
-        print(graph)
+        print('-' * 45)
         py.image.save_as(graph, filename='my_mrp/static/popular_models.png')
-        print(graph)
-
         py.image.ishow(graph)
-
     except:
-        return False
         print('[!] FATAL ERROR')
+        return False
     finally:
         print('[*] Finished Executing PLOTLY/DB Queries.')
-        print('----------------------------------------------')
+        print('-' * 45)
 
-createGraph()
+
+class HomeView(TemplateView):
+    """Make the HomeView class."""
+    template_name = 'generic/home.html'
+    context_object_name = 'quotes'
+
+    def get_context_data(self):
+        createGraph()
+        return {}
